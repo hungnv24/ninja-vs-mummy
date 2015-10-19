8e9cc5d28bf9fedee3260b5fda1a142514c3d3a8
Hung NV <hungnv92@gmail.com>
2015-10-19 23:26:07 +0700

    add google analystics

---
+++ Assets/Scripts/GoogleAnalystics.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoogleAnalystics : MonoBehaviour
+{
+    public GoogleAnalyticsV3 gav3;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
